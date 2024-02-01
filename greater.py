79d3e9bcc51912877c9ad4e524a47e67a2e7b79d
archit5122 <155718804+rathodarchit@users.noreply.github.com>
2024-02-01 13:31:43 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,9 @@
+a=30
+b=20
+
+if a>b:
+    print("A is greater")
+elif b>a:
+    print("B is greater")
+else:
+    print("Both values are similar")
---
+++ result.py
@@ -0,0 +1,27 @@
+m1=int(input("Enter Marks 1: "))
+m2=int(input("Enter Marks 2: "))
+m3=int(input("Enter Marks 3: "))
+
+if m1>=28 and m2>=28 and m3>=28:
+    total=m1+m2+m3
+    per=total/3
+    result="PASS"
+
+    if per>=80:
+        grade="A"
+    elif per>=70:
+        grade="B"
+    elif per>=60:
+        grade="C"
+    else:
+        grade="D"
+else:
+    total=m1+m2+m3
+    per="***"
+    result="FAIL"
+    grade="F"
+
+print("Total:",total)
+print("Percentage:",per)
+print("Result:",result)
+print("Grade:",grade)
